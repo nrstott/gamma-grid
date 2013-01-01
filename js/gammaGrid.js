eb18afdfa5c819df1a7153619a0652260cec00e3
Martin Murphy <martin.murphy@whiteboard-it.com>
2013-01-01 15:59:19 -0600

    Fixed unclosed tag

--- js/gammaGrid.js
+++ js/gammaGrid.js
@@ -1,6 +1,23 @@
+/* Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
 (function( $ ) {
-
-
   $.fn.gammaGrid = function(options) {
 
     function hashToQuery(hash){
@@ -33,7 +50,7 @@
     queryHash.skip = start - options.pageSize -1 ;     
     queryHash.skip = queryHash.skip < 0   ? 0 : queryHash.skip;
     var prev = start !==1    ? "<a href='" + hashToQuery(queryHash)+ "'>&lt;&mdash;&nbsp;Previous </a>" : "";
-    return "<div class='gammaPager'>" + prev + " Showing " + start + " to " + end + " of " + count + next + "<div>";
+    return "<div class='gammaPager'>" + prev + " Showing " + start + " to " + end + " of " + count + next + "</div>";
    };
    var query = window.location.search;
    var dataFormatters = options.dataFormatters || {};
